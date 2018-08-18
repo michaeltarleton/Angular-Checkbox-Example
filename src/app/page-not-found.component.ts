3f98ddd2e68dde2484e72769c317115c8daaad9a
Michael Tarleton <michael.tarleton@me.com>
2018-08-18 00:12:27 -0500

    master: Updated page not found

--- src/app/page-not-found.component.ts
+++ src/app/page-not-found.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   template: `
-    <p>
-      404 Page Not Found
-    </p>
+    <div>
+      <mat-card>
+        <mat-card-title><h1>404 Not Found</h1></mat-card-title>
+        <mat-card-content>
+          <p>  
+            Could not find the path: <b>{{path}}</b>
+          </p>
+
+          <p>
+            <i>(You will be redirected in {{redirectTime / 1000}} seconds)</i>
+          </p>
+
+        </mat-card-content>
+      </mat-card>
+    </div>
   `,
-  styles: []
+  styleUrls: ['./page-not-found.component.scss']
 })
 
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  path: string
+  redirectTime: number = 2000
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.path = this.route.snapshot.url.join('')
+    setTimeout(() => {this.router.navigate([''])}, 2000)
   }
 
 }
